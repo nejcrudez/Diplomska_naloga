59298b116ba127a75222f09c1fd2a6a655cd526f
nejcrudez <rudeznejc@gmail.com>
2025-08-10 12:40:40 +0200

    Add files via upload

--- testGS.m
+++ testGS.m
@@ -1,20 +1,20 @@
-% testGS_Hilbert5.m
+
 clear; clc;
 
-% 1) Sestavi Hilbertovo matriko in desno stran
+% 1) Ustvarimo Hilbertovo matriko velikosti 3 in desno stran
 n     = 3;
-L     = hilb(n);        % Hilbertova matrika 5×5 (SPD)
-f     = (1:n)';         % npr. f = [1;2;3;4;5]
-omega = 1;              % Gauss–Seidel
+L     = hilb(n);        % Hilbertova matrika 3×3 (simetrična pozitivno definitna)
+f     = (1:n)';         % npr. f = [1;2;3]
+omega = 1;              % osnovni Gauss–Seidel
 
 % 2) Začetni približek
 u = zeros(n,1);
 
-% 3) Naredi nekaj GS korakov
+% 3) 5 GS korakov
 fprintf('Iteracija   u (vektor)\n');
 for k = 1:5
     u = relaxGaussSeidel(L, u, f, omega);
-    fprintf('%2d     [', k);
+    fprintf('%2d     [', k);  % izpis na 6 decimalnih mest natančno
     fprintf(' %8.6f', u);
     fprintf(' ]\n');
 end
